6a3adea33d7f0971ff5b837977c48cbc0ba276cc
packy周 <packyzhou@icloud.com>
2014-08-10 15:56:49 +0800

    create GestureRecognizer code demo

---
+++ GestureRecognizerDemo/GestureRecognizerDemoTests/GestureRecognizerDemoTests.m
@@ -0,0 +1,34 @@
+//
+//  GestureRecognizerDemoTests.m
+//  GestureRecognizerDemoTests
+//
+//  Created by 周经伟 on 14-8-10.
+//  Copyright (c) 2014年 packy. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface GestureRecognizerDemoTests : XCTestCase
+
+@end
+
+@implementation GestureRecognizerDemoTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
